3da0a060de66c717c3473c1b68074edeb8d2db81
Parsa-19 <parsakhalife19@gmail.com>
2025-10-18 15:53:59 +0330

    impove

--- color_editor.py
+++ color_editor.py
@@ -1,26 +1,29 @@
 from colorama import Fore, Style, init
 import os
+import keyboard
 
 init(autoreset=True)
 
-import keyboard
-
+all_ability_keys_str = '123456eftrgyq'
+movement_keys = ['w', 's', 'd', 'a', 'space']
+backspace_str = 'backspace'
 
 def on_key_press(event):
+	global all_ability_keys_str, movement_keys, backspace_str
 
 	key = event.name
 	clear = lambda: os.system('cls')
 
 	# movment
-	if key in ['w', 's', 'd', 'a', 'space']:
+	if key in movement_keys:
 		print(Fore.GREEN + key + Style.RESET_ALL, end=' ', flush=True)
 
 	# ability
-	elif key in '1234eftrgyq5':
+	elif key in all_ability_keys_str:
 		print(Fore.BLUE + key + Style.RESET_ALL, end='', flush=True)
 
 	# clear
-	elif key == 'backspace':
+	elif key == backspace_str:
 		clear()
 
 	# key does not defined
@@ -28,11 +31,14 @@ def on_key_press(event):
 		print(Fore.RED + key + Style.RESET_ALL, end='\n', flush=True)
 
 if __name__ == "__main__":
-	print(f"""'esc' -> exit
-{Fore.BLUE}ABILITY = 1, 2, 3, 4, e, f, t, r, g, y, q, 5
-{Fore.GREEN}MOVMENT & JUMP = W, A, S, D, 'space'
+	print(f'''
+\'esc\' -> exit
+\'backspace\' -> clear
+
+{Fore.BLUE}ABILITIES = {', '.join([key.upper() for key in all_ability_keys_str])}
+{Fore.GREEN}MOVMENT & JUMP = {', '.join([key.upper() for key in movement_keys])}
 {Fore.RED}NOT defined keys
-		""")
+		''')
 
 	keyboard.on_press(on_key_press)
 	keyboard.wait('esc')
